d4eaf7e39d2ac966035732ab4c73aa204d6f235d
BuddhikaRJ <buddhikarj@gmail.com>
2023-01-12 12:53:15 +0530

    Keyboard input

--- src/GameManager.ts
+++ src/GameManager.ts
@@ -1,11 +1,14 @@
 import { Container, Loader, Rectangle, Sprite } from "pixi.js";
 import { CONFIG } from "./gameConfig";
+import { Keyboard } from "./Keyboard";
 
 export class GameManager {
     gameWorld: Container;
     assetLoader: Loader;
     stage: Container;
     testSprite: Sprite;
+
+    upKey : Keyboard;
 
     constructor(_assets: Loader, _stage: Container) {
         this.gameWorld = new Container();
@@ -15,6 +18,10 @@ export class GameManager {
         this.testSprite = new Sprite(this.assetLoader.resources[CONFIG.ASSETS.game_sprite].texture);
         this.testSprite.texture.frame = new Rectangle(...CONFIG.TEXTURE_COORDS.SQUARE)
         this.gameWorld.addChild(this.testSprite);
+
+        this.upKey = new Keyboard(" ", ()=>{console.log("press")}, ()=>{console.log("release");
+        })
+        
     }
 
     update(dt: number){
---
+++ src/Keyboard.ts
@@ -0,0 +1,54 @@
+export class Keyboard {
+    value : String;
+    isDown : boolean;
+    isUp : boolean;
+    press : Function;
+    release : Function;
+    downListener : any;
+    upListener : any;
+
+    constructor(key : String, pressFunc : Function, releaseFunc : Function){
+        this.value = key;
+        this.isDown = false;
+        this.isUp = true;
+        this.press = pressFunc;
+        this.release = releaseFunc;
+
+        //Attach event listeners
+        this.downListener = this.downHandler.bind(this);
+        this.upListener = this.upHandler.bind(this);
+
+        window.addEventListener("keydown", this.downListener, false);
+        window.addEventListener("keyup", this.upListener, false);
+
+        console.log(this);
+        
+    }
+
+    downHandler(event: KeyboardEvent) {
+        if (event.key === this.value) {
+            if (this.isUp && this.press) {
+                this.press();
+            }
+            this.isDown = true;
+            this.isUp = false;
+            event.preventDefault();
+        }
+    };
+
+    upHandler(event: KeyboardEvent) {
+        if (event.key === this.value) {
+            if (this.isDown && this.release) {
+                this.release();
+            }
+            this.isDown = false;
+            this.isUp = true;
+            event.preventDefault();
+        }
+    };
+
+    unsubscribe(){
+        window.removeEventListener("keydown", this.downListener);
+        window.removeEventListener("keyup", this.upListener);
+    };
+}
